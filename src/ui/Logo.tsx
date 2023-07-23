1e542544eec707805e56bb3e98b3c91f7dd560e0
Manu Roshardt <manuelroshardt@gmail.com>
2023-07-23 19:33:48 -0400

    added main images, coverted logo to svg using figma and created a component for it

---
+++ src/ui/Logo.tsx
@@ -0,0 +1,11 @@
+import WildOasisLogo from "./WildOasisLogo";
+
+const Logo = () => {
+  return (
+    <div>
+      <WildOasisLogo className="w-28" />
+    </div>
+  );
+};
+
+export default Logo;
--- src/ui/SideBar.tsx
+++ src/ui/SideBar.tsx
@@ -1,7 +1,9 @@
+import Logo from "./Logo";
+
 const SideBar = () => {
   return (
-    <aside className="bg-gray-50 py-12 px-9 border-r border-gray-100 w-96">
-      SideBar
+    <aside className="bg-gray-50 py-12 px-9 border-r border-gray-100 w-96 flex flex-col items-center">
+      <Logo />
     </aside>
   );
 };
