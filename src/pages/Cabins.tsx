143cbf26b29b92609c7e7d26f56cb9f85f0b9e77
Manu Roshardt <manuelroshardt@gmail.com>
2023-07-24 20:03:47 -0400

    added supabase setup - created tables, added client to project

--- src/pages/Cabins.tsx
+++ src/pages/Cabins.tsx
@@ -1,5 +1,17 @@
+import { getCabins } from "@/services/apiCabins";
+import { useEffect } from "react";
+
 const Cabins = () => {
-  return <div>Cabins</div>;
+  useEffect(() => {
+    getCabins().then((data) => console.log(data));
+  }, []);
+
+  return (
+    <div>
+      Cabins
+      <img src="https://vuaaywhtormaxenfkvga.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg" />
+    </div>
+  );
 };
 
 export default Cabins;
